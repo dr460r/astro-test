9934e109c2c5b0e10e874f336491ea711fba7669
Dragor Draganović <dragor.draganovic@gmail.com>
2023-09-24 02:27:12 +0200

    Setup Astro

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://dr460r.github.io',
+	base: '/astro-test',
+	integrations: [mdx(), sitemap()],
+});
